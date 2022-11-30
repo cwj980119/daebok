53ae63cc910a27e54be9d3b3b4006f8a096c3669
cwj980119 <cwj980119@g.hongik.ac.kr>
2022-11-30 20:51:05 +0900

    ExpansionPanel로 수정

--- lib/estimate.dart
+++ lib/estimate.dart
@@ -21,63 +21,82 @@ class Estimate extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        child: GetBuilder<EstimateController>(builder: (controller) {
-          return Column(
-            children: [
-              Container(
-                height: 50,
-                color: Colors.green,
-                child: Center(
-                  child: Text(
-                    'Print / Make Book',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ),
-              ),
-              Column(
-                children: [
-                  Center(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Size', style: TextStyle(fontSize: 20),),
-                          DropdownButton(
-                            value: controller.size,
-                            items: controller.size_list.map((value) {
-                              return DropdownMenuItem(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                            onChanged: (value) {
-                              controller.setSize(value);
-                            },
+        child: GetBuilder<EstimateController>(
+          builder: (controller) {
+            return ExpansionPanelList(
+              animationDuration: Duration(milliseconds: 500),
+              children: [
+                ExpansionPanel(
+                    isExpanded: controller.isExpanded,
+                    canTapOnHeader: true,
+                    headerBuilder: (context, isExpanded) {
+                      return Container(
+                        height: 50,
+                        color: Colors.green,
+                        child: Center(
+                          child: Text(
+                            'Print / Make Book',
+                            style: TextStyle(fontSize: 25),
                           ),
-                          Text('Quantity', style: TextStyle(fontSize: 20),),
-
-                          SizedBox(
-                            width: 50,
-                            child: TextField(
-                              inputFormatters: [FilteringTextInputFormatter.allow(RegExp('[0-9]')),],
-                              keyboardType: TextInputType.number,
-                              onChanged: (value){
-                                if(value == "") return;
-                                controller.setQuantity(int.parse(value));
-                              },
-
+                        ),
+                      );
+                    },
+                    body: Column(
+                      children: [
+                        Center(
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Size',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                DropdownButton(
+                                  value: controller.size,
+                                  items: controller.size_list.map((value) {
+                                    return DropdownMenuItem(
+                                      value: value,
+                                      child: Text(value),
+                                    );
+                                  }).toList(),
+                                  onChanged: (value) {
+                                    controller.setSize(value);
+                                  },
+                                ),
+                                Text(
+                                  'Quantity',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                SizedBox(
+                                  width: 50,
+                                  child: TextField(
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.allow(
+                                          RegExp('[0-9]')),
+                                    ],
+                                    keyboardType: TextInputType.number,
+                                    onChanged: (value) {
+                                      if (value == "") return;
+                                      controller.setQuantity(int.parse(value));
+                                    },
+                                  ),
+                                )
+                              ],
                             ),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          );
-        }),
+                          ),
+                        )
+                      ],
+                    ))
+              ],
+              expandedHeaderPadding: EdgeInsets.all(0),
+              expansionCallback: (index, isExpanded) {
+                controller.toggleExpand();
+              },
+            );
+          },
+        ),
       ),
     );
   }
--- lib/estimate_controller.dart
+++ lib/estimate_controller.dart
@@ -1,13 +1,20 @@
-import 'package:get/get.dart';
+ import 'package:get/get.dart';
 
 class EstimateController extends GetxController{
 
+  RxBool isBook = false.obs;
   List size_list = ['A4,B5','A3,B4','A4,B5(색지)','A3,B4(색지)','A4,B5(클린지)','A3,B4(클린지)'];
   late String size = size_list[0];
 
   List quantity_list = [];
   int quantity = 0;
 
+  bool isExpanded = true;
+
+  toggleExpand(){
+    isExpanded = !isExpanded;
+    update();
+  }
 
   setSize(value){
     size = value.toString();
